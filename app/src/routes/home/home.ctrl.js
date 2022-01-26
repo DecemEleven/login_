8d9704d72a4e4588925195e0eb021c295642cfa3
DecemEleven <jjp011211@naver.com>
2022-01-26 04:51:25 +0000

    backend

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -9,11 +9,32 @@ const output = {
 	},
 };
 
+const users = {
+	id: ["jjp011211", "기모찌", "팀장님"],
+	pw: ["1234", "12345", "123456"],
+};
+
 const process = {
 	login: (req, res) => {
-		console.log(req.body);
+		const id = req.body.id
+			pw = req.body.pw;
+		
+		if (users.id.includes(id)) {
+			const idx = users.id.index0f(id);
+			if (users.pw[idx] === pw) {
+				return res.json({
+				success: true,
+				});
+			}
+		}
+		
+		return res.json({
+			success: false,
+			msg: "로그인에 실패하셨습니다.",
+		});
 	},
 };
+
 
 module.exports = {
 	output,
